f0509d12b53a601ffebd04a2a1409298fcb8bcf7
Karl christian <karl.christian@careem.com>
2022-06-30 23:23:38 +0700

    changes

---
+++ dags/sql/air_quality.sql
@@ -0,0 +1,11 @@
+SELECT
+    TIMESTAMP_TRUNC(timestamp, month) AS month_key,
+    country,
+    city,
+    pollutant,
+    AVG(value) AS avg_value,
+    unit,
+    source_name
+FROM `bigquery-public-data.openaq.global_air_quality`
+WHERE EXTRACT(YEAR FROM timestamp) >= 2020
+GROUP BY 1,2,3,4,6,7
